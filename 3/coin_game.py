742ccb1735a12a20d9da89ffc756164b7814cedd
Harryone122 <harryone122@gmail.com>
2023-06-21 18:58:58 +0800

    complete

--- 3/coin_game.py
+++ 3/coin_game.py
@@ -5,8 +5,9 @@ import random
 pygame.init()
 
 # 視窗寬度和高度
-window_width = 800
-window_height = 600
+screen_width = 800
+screen_height = 600
+screen = pygame.display.set_mode((screen_width, screen_height))
 
 # 定義顏色
 white = (255, 255, 255)
@@ -14,20 +15,22 @@ blue = (0, 0, 255)
 yellow = (255, 255, 0)
 
 # 定義主角尺寸和移動速度
+player_image = pygame.image.load('character.png')
 player_size = 50
 player_speed = 10
 
 # 定義金幣尺寸和移動速度
+coin_image = pygame.image.load('coin.png')
 coin_size = 30
 coin_speed = 5
 
 # 初始化視窗
-window = pygame.display.set_mode((window_width, window_height))
+window = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("吃金幣遊戲")
 
 # 初始化遊戲時的位置和分數
-player_x = window_width // 2
-player_y = window_height // 2
+player_x = screen_width// 2
+player_y = screen_height // 2
 score = 0
 
 # 創建字體
@@ -35,23 +38,20 @@ font = pygame.font.Font(None, 36)
 
 def generate_coin():
     """隨機生成金幣的位置"""
-    coin_x = random.randint(0, window_width - coin_size)
-    coin_y = random.randint(0, window_height - coin_size)
+    coin_x = random.randint(0,screen_width - coin_size)
+    coin_y = random.randint(0, screen_height - coin_size)
     return coin_x, coin_y
 
 def draw_player(x, y):
     """繪製主角"""
-    player_image = pygame.image.load('charactor.png')
-    window.blit(player_image, (x, y))
+    screen.blit(player_image, (x, y))
 
 def draw_coin(x, y):
     """繪製金幣"""
-    coin_image = pygame.image.load('coin.png')  
-    window.blit(coin_image, (x, y))
-
+    screen.blit(coin_image, (x, y))
 def display_score(score):
     """顯示分數"""
-    score_text = font.render("分數: " + str(score), True, white)
+    score_text = font.render("score: " + str(score), True, white)
     window.blit(score_text, (10, 10))
 
 def collision_detection(player_x, player_y, coin_x, coin_y):
